25fc8dcf6b4996cbdb370617c2b06f01e0f8300f
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-08-26 16:33:37 +0300

    vanilla js simple files project
    very simple just js app

---
+++ vanilla-js-simple/app.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector('button');
+
+let modal;
+let backdrop;
+
+btn.addEventListener('click', showModalHandler);
+
+function showModalHandler() {
+  if (modal) {
+    return;
+  }
+
+  modal = document.createElement('div');
+  modal.className = 'modal';
+
+  const modalText = document.createElement('p');
+  modal.textContent = 'Are you sure?';
+
+  const modalCancelAction = document.createElement('button');
+  modalCancelAction.textContent = 'Cancel';
+  modalCancelAction.className = 'btn btn--alt';
+  modalCancelAction.addEventListener('click', closeModalHandler);
+
+  const modalConfirmAction = document.createElement('button');
+  modalConfirmAction.textContent = 'Confirm';
+  modalConfirmAction.className = 'btn';
+  modalConfirmAction.addEventListener('click', closeModalHandler);
+
+  modal.append(modalText);
+  modal.append(modalCancelAction);
+  modal.append(modalConfirmAction);
+
+  document.body.append(modal);
+
+  backdrop = document.createElement('div');
+  backdrop.classList = 'backdrop';
+
+  backdrop.addEventListener('click', closeModalHandler);
+
+  document.body.append(backdrop);
+}
+
+function closeModalHandler() {
+  modal.remove();
+  modal = null;
+
+  backdrop.remove();
+  backdrop = null;
+}
