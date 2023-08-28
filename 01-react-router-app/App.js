517cea0e835de713db2065c9efb45f0a8246851f
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-08-28 16:15:32 +0300

    Add files via upload

---
+++ 01-react-router-app/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetups from './pages/AllMeetups';
+import NewMeetup from './pages/NewMeetup';
+import Favorities from './pages/Favorities';
+import Layout from './components/layout/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetups />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetup />
+        </Route>
+        <Route path='/favorities'>
+          <Favorities />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
--- 01-react-router-app/index.js
+++ 01-react-router-app/index.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
+import './index.css';
+import App from './App';
+import { FavoritiesContextProvider } from './store/favorities-context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FavoritiesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritiesContextProvider>
+  </React.StrictMode>
+);
