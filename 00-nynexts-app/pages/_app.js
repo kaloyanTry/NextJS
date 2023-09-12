597fcfc2b60fd9d691c912469f793f86fac72f99
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-09-12 15:39:34 +0300

    Add files via upload

--- 00-nynexts-app/pages/_app.js
+++ 00-nynexts-app/pages/_app.js
@@ -1,12 +1,5 @@
-import Layout from '../components/layout/layout';
-import '../styles/global.css';
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- 00-nynexts-app/pages/index.js
+++ 00-nynexts-app/pages/index.js
@@ -1,12 +1,18 @@
+import Link from 'next/link';
+
+import EvetnsList from '@/components/events-components/events-list';
 import { getAllEvents } from '../data-events';
-import EventsList from '../components/events/events-list';
 
 function HomePage() {
   const events = getAllEvents();
+
   return (
     <div>
-      {/* <h1>Home Page</h1> */}
-      <EventsList events={events} />
+      <h1>Home Page</h1>
+      <div>
+        <Link href='/events'>Filter the evetns</Link>
+      </div>
+      <EvetnsList events={events} />
     </div>
   );
 }
