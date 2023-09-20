0c794be5506134bc72b31b5468643b06e6c99a11
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-09-20 10:06:18 +0300

    Add files via upload

---
+++ 04-data-fetching/pages/[uid].js
@@ -0,0 +1,16 @@
+function UserIdPage(props) {
+  return <div>{props.id}</div>;
+}
+
+export default UserIdPage;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const userId = params.uid;
+
+  return {
+    props: {
+      id: 'userid-' + userId,
+    },
+  };
+}
