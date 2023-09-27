b3b5fd1fee755de754e72734bd05fc0aa9dacc88
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-09-27 09:28:28 +0300

    Delete 06-prj-optimization/pages/components directory

--- 06-prj-optimization/pages/components/event-detail/event-content.js
+++
@@ -1,7 +0,0 @@
-import styles from './event-content.module.css';
-
-function EventContent({ children }) {
-  return <section className={styles.content}>{children}</section>;
-}
-
-export default EventContent;
