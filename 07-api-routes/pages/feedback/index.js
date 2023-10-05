d1a0cf5f9301cdce349510634adaaca35853eed3
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-10-05 10:53:22 +0300

    Add files via upload

---
+++ 07-api-routes/pages/feedback/index.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+
+function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    // fetch('/api/' + id); = the same:
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+  return (
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
+
+//that is how we should interact with api routes when using that data for prerenderng a page: execute the code directly in getStaticProps() or getServerSideProps().
